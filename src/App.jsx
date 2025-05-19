cd36de9be2ce6bd14ae1e2b7399ff316a5bf2cd2
Mass Mass <codehub65@gmail.com>
2025-05-19 00:51:42 -0500

    clean up code
    Remove the user_auth

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Header from "./Components/Header.jsx";
 import Hero from "./Components/Hero.jsx"
 import Footer from "./Components/Footer.jsx"
 
-// import UserAuth from "./UserLogin/UserAuth.jsx";
 function App() {
   const [darkMode, setDarkMode] = useState(false);
 
@@ -13,7 +12,6 @@ function App() {
     <div className={`app ${darkMode ? "bg-dark text-white" : "bg-light text-dark"}`}>
       <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <Hero darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
-      {/* <UserAuth /> */}
       <Footer darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
     </div>
   );
